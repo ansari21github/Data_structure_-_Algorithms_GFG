70fc1fc9974dd237992751fe0fc2a2101b1ab066
Md. Rashid Ansari <133353728+ansari21github@users.noreply.github.com>
2025-09-07 14:56:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.js
@@ -0,0 +1,9 @@
+class Solution {
+    // Function to remove duplicates from the given array.
+    removeDuplicates(arr) {
+        return Array.from(new Set(arr));
+        
+        
+        
+    }
+}
